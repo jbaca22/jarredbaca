22852cf91534fa8a96889f9a34bca2fc6b465e14
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-09-24 14:20:37 -0600

    gave header sticky property for floating navbar

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,15 @@ function About() {
     return (
       <section>
         <h1 id="about">Who am I?</h1>
-        <img src={coverPhoto} className="my-2" style={{ width: "100%" }}  alt="cover" />
+        <img src={coverPhoto} className="my-2" style={{ width: "50" }}  alt="cover" />
+        <div className="flex-row">
+          <p className="about-description">
+            I am a recent marketing grad/ full-stack web developer. 
+            I have excellent sales skills, marketing knowledge, and know multiple front end/ back end coding languages. 
+            I am interested in a full time position where I can cultivate success and make a difference with my unique talents.
+            Check out some of my work above!
+          </p>
+        </div>
       </section>
     );
 }
